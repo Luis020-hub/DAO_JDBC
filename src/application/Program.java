84617abf2ecb660e371a2bbcd457a15f099d4ca3
Luis020-hub <guhmonteiro10@hotmail.com>
2023-08-27 19:45:38 -0300

    27/08/2023
    - Criando a entidade Department com construtores, getters e setters.
    - Chamando o Department na aplicação principal.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,50 +1,12 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
-import db.DbException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println();
-
-		Connection conn = null;
-		Statement st = null;
-		try {
-			conn = DB.getConnection();
-
-			conn.setAutoCommit(false);
-
-			st = conn.createStatement();
-
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
-
-			/*
-			 * int x = 1; if (x < 2) { throw new SQLException("Fake error"); }
-			 */
-
-			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
-
-			conn.commit();
-
-			System.out.println("Update rows(id 1): " + rows1);
-			System.out.println("Update rows(id 2): " + rows2);
-
-		} catch (SQLException e) {
-			try {
-				conn.rollback();
-				throw new DbException("Transaction failed! Caused by: " + e.getMessage());
-			} catch (SQLException e1) {
-				throw new DbException("Error trying to rollback! caused by: " + e1.getMessage());
-			}
-		} finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
 	}
 }
