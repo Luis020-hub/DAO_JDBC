a06e095e222007ccf3bd609d592baa975744134b
Luis020-hub <guhmonteiro10@hotmail.com>
2023-08-27 20:00:01 -0300

    27/08/2023
    - Correção DepartmentDao > SellerDao.

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,16 +2,16 @@ package model.dao;
 
 import java.util.List;
 
-import model.entities.Department;
+import model.entities.Seller;
 
-public interface DepartmentDao {
-	void insert(Department obj);
+public interface SellerDao {
+	void insert(Seller obj);
 
-	void update(Department obj);
+	void update(Seller obj);
 
 	void deleteById(Integer id);
 
-	Department findById(Integer id);
+	Seller findById(Integer id);
 
-	List<Department> findAll();
+	List<Seller> findAll();
 }
