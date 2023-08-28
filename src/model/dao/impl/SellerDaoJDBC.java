0c629f08fbdeb0bcd43d1009f04c21371c95199c
Luis020-hub <guhmonteiro10@hotmail.com>
2023-08-28 19:35:04 -0300

    28/08/2023
    - Deletando os sellers com o método deleteById.

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -60,8 +60,8 @@ public class SellerDaoJDBC implements SellerDao {
 	public void update(Seller obj) {
 		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement("UPDATE seller "
-					+ "SET Name = ?, Email = ?, BaseSalary = ?, DepartmentId = ? " + "WHERE Id = ?");
+			st = conn.prepareStatement(
+					"UPDATE seller " + "SET Name = ?, Email = ?, BaseSalary = ?, DepartmentId = ? " + "WHERE Id = ?");
 
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
@@ -79,8 +79,18 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
 
+			st.setInt(1, id);
+
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
